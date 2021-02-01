e013df4f357db311ca8e81f86f9fdd479f6d6140
pini85 <40143951+pini85@users.noreply.github.com>
2021-02-01 13:56:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const lightSwitch = document.querySelector(".light-switch");
 
 //attaching an event listener to that element. Each time the user clicks the element the callback function gets executed
 lightSwitch.addEventListener("click", () => {
-  //toggling a class "on" to the element
+  //toggling a class "on" to the element, this will toggle an additional class called "on" to the lightSwitch element.
   lightSwitch.classList.toggle("on");
 
   //toggling a class "dark-mode" to an element. What element should it be?
